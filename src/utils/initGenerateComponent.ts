34b41dd307b5200a901defa1be962c0050a2005c
Younyi Kim <rlakuku1221@gmail.com>
2024-08-22 21:56:35 +0900

    chore: tsx 설치, dev 스크립트 추가 및 tsdoc 추가

--- src/utils/initGenerateComponent.ts
+++ src/utils/initGenerateComponent.ts
@@ -6,6 +6,14 @@ import { yamlParser } from './yamlParser';
 import { generateComponents } from './generateComponents';
 import { generateTypeDefinitionFile } from './generateTypeDefinitionFile';
 
+/**
+ * CLI 명령을 초기화하고 YAML 파일을 기반으로 React 컴포넌트와 TypeScript 타입 정의 파일을 생성하는 함수.
+ *
+ * @param program - `commander` 라이브러리에서 제공하는 `Command` 인스턴스
+ *
+ * @throws {Error} - 파일 경로가 잘못되었거나 YAML 파일의 파싱 또는 컴포넌트/타입 생성 과정에서 오류가 발생할 경우 오류를 던집니다.
+ */
+
 export function initGenerateComponent(program: Command) {
   program
     .version('1.0.0')
