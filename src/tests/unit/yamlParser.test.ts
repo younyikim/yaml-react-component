7fd623540def633872be497c77669eecc846bac2
Younyi Kim <rlakuku1221@gmail.com>
2024-08-22 14:09:10 +0900

    chore: test 폴더 위치 변경

---
+++ src/tests/unit/yamlParser.test.ts
@@ -0,0 +1,15 @@
+import { yamlParser } from '../../utils/yamlParser';
+import * as path from 'path';
+
+describe('YAML Parser', () => {
+  const sampleYamlPath = path.join(
+    __dirname,
+    '../../config/sample-config.yaml'
+  );
+
+  test('should parse YAML file to JSON object', () => {
+    const result = yamlParser(sampleYamlPath);
+    console.log('Parsed YAML result:', JSON.stringify(result, null, 2)); // JSON 객체를 읽기 쉬운 형식으로 출력
+    expect(result).toHaveProperty('components');
+  });
+});
