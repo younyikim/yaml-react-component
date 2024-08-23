6e8453e45222916dd07771cdd830ca64f4c957b8
Younyi Kim <rlakuku1221@gmail.com>
2024-08-23 20:29:18 +0900

    chore: src/index.ts export 모듈 추가

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,5 @@ import { initGenerateComponent } from './utils/initGenerateComponent';
 
 initGenerateComponent(program);
 program.parse(process.argv);
+
+export { eventBus } from './utils/eventBus';
